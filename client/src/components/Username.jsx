96a701f134ea91e4ea734ccb0bc54b001b17e56d
kritsanapr <kritsana.pr.41@gmail.com>
2023-02-26 21:19:35 +0700

    Commit username/login page

--- client/src/components/Username.jsx
+++ client/src/components/Username.jsx
@@ -6,7 +6,7 @@ import styles from "../styles/Username.module.css";
 function Username() {
   return (
     <div className="container mx-auto">
-      <div className="flex  h-screen">
+      <div className="flex  justify-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
             <h4 className="text-5xl font-bold text-grey-500">Hello Again!</h4>
@@ -17,18 +17,24 @@ function Username() {
 
           <form className="py-1">
             <div className="profile flex justify-center py-4">
-              <img src={avatar} alt="avatar" />
+              <img src={avatar} className={styles.profile_img} alt="avatar" />
             </div>
 
             <div className="textbox flex flex-col items-center">
-              <input type="text" placeholder="Username" />
-              <button type="submit">Let's Go</button>
+              <input
+                className={styles.textbox}
+                type="text"
+                placeholder="Username"
+              />
+              <button className={styles.btn} type="submit">
+                Let's Go
+              </button>
             </div>
 
             <div className="text-center py-4">
               <span className="text-grey-500">
                 Not a member
-                <Link className="text-red-500" to={"/register"}>
+                <Link className="text-red-500 px-1" to={"/register"}>
                   Register now
                 </Link>
               </span>
